488d1051cdbbf4e0b9aacabfb74c9054f4e33b65
Jean C Sanchez Adon <jean.sanchez.proxy@gmail.com>
2025-08-18 22:02:47 -0400

    merge completed

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a61e9f730f3c6e8e77bc81b6cb49117f9b5596c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b7e3a191f36bacece93ef6be11681667f6905db")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
